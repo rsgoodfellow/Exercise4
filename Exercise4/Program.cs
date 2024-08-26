1e0b4e81e5910e157e2fc8023d6346964278aa40
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-26 12:39:55 -0500

    Add project files.

---
+++ Exercise4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int length;
+            int width;
+
+            Console.Write("Enter the length of the rectangle: ");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the width of the rectangle: ");
+            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+            int area = length * width;
+            int perimeter = length * 2 + width * 2;
+            Console.WriteLine("Results:");
+            Console.WriteLine("Area of the rectangle: " + area);
+            Console.WriteLine("Perimeter of the rectangle: " + perimeter);
+            Console.ReadKey();
+        }
+    }
+}
