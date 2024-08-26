4d46b28e14f351bb433fbb08d087f964dbb6dfba
rsgoodfellow <rsgoodfellow@amarillocollege.com>
2024-08-26 12:50:41 -0500

    Update Program.cs
    added comments

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -10,20 +10,20 @@ namespace Exercise4
     {
         static void Main(string[] args)
         {
-            int length;
-            int width;
+            int length; //declare length variable
+            int width; //declare width variable
 
-            Console.Write("Enter the length of the rectangle: ");
-            length = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Enter the width of the rectangle: ");
-            width = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("");
-            int area = length * width;
-            int perimeter = length * 2 + width * 2;
-            Console.WriteLine("Results:");
-            Console.WriteLine("Area of the rectangle: " + area);
-            Console.WriteLine("Perimeter of the rectangle: " + perimeter);
-            Console.ReadKey();
+            Console.Write("Enter the length of the rectangle: "); //asks for user input
+            length = Convert.ToInt32(Console.ReadLine()); //converts user input to int and stores it as length
+            Console.Write("Enter the width of the rectangle: "); //asks for user input
+            width = Convert.ToInt32(Console.ReadLine()); //converts user input to int and stores it as width
+            Console.WriteLine(""); //enters blank line
+            int area = length * width; //calculates area
+            int perimeter = length * 2 + width * 2; //calculates perimeter
+            Console.WriteLine("Results:"); //displays text
+            Console.WriteLine("Area of the rectangle: " + area); //displays area of the rectangle
+            Console.WriteLine("Perimeter of the rectangle: " + perimeter); //displays rectangle perimeter
+            Console.ReadKey(); //displays output
         }
     }
 }
